290aa4a50ec69381152e7401e49b289e14fcd473
datasnowman <darsch@microsoft.com>
2020-08-18 13:17:19 -0700

    packageAdded

--- sampleCode/Hello/HelloWorld.java
+++ sampleCode/Hello/HelloWorld.java
@@ -1,3 +1,5 @@
+package sampleCode.Hello;
+
 public class HelloWorld {
     public static void main (String [] args) {
         System.out.println("HelloWorld");
--- studentWork/DrawBoxes/DrawBoxes.java
+++ studentWork/DrawBoxes/DrawBoxes.java
@@ -1,3 +1,5 @@
+package studentWork.DrawBoxes;
+
 public class DrawBoxes {
     public static void main (String[] args){
         System.out.println("+------+");
