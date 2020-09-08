b0e4f87ff11866481a065eb887e01eb99bd63a25
Darwin Schweitzer <darwinteals@outlook.com>
2020-09-08 20:34:10 +0000

    ExChpt1Quizz

---
+++ sampleCode/Chapter1/OutputExample.java
@@ -0,0 +1,8 @@
+package sampleCode.Chapter1;
+
+public class OutputExample {
+    public static void main (String[] args) {
+       System.out.print("This is");
+       System.out.println("Not what I expected");
+       System.out.print("The output to be!");   }
+  } // end of class
