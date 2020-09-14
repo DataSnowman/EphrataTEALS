44764a6d5de05a83126304e7ae5fbe437828d91d
Brian Rogers <brian-dot-net@outlook.com>
2020-09-14 11:24:05 -0700

    Add Ephrata school song example

---
+++ sampleCode/Chapter1/SchoolSong.java
@@ -0,0 +1,13 @@
+package sampleCode.Chapter1;
+
+public class SchoolSong {
+    public static void main(String[] args) {
+        System.out.println("Fight on, fight on, Ephrata Tigers fight");
+        System.out.println("Fight on, fight on, we'll show our Tiger's might");
+        System.out.println("Fight on, fight on, it's victory tonight");
+        System.out.println("Fight on, fight on, Ephrata Tigers fight");
+        System.out.println("E - P - H - R - A - T - A");
+        System.out.println("Fight on, fight on, it's victory tonight");
+        System.out.println("Fight on, fight on, Ephrata Tigers fight");
+    }
+}
