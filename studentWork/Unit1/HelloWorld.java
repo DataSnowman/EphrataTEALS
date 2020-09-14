461d21d4a70c84b5af39cd4d248bc7c3f24b7f4b
carolinedanzi <cdanzi@ephrataschools.org>
2020-09-14 16:00:19 +0000

    Add HelloWorld.java

---
+++ studentWork/Unit1/HelloWorld.java
@@ -0,0 +1,11 @@
+package studentWork.Unit1;
+
+// studentWork -> Unit1 -> HelloWorld.java
+public class HelloWorld {
+
+    // Main method - program starts running here
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
+}
