e60140e5dfa07d2149498de70deabfe3a07cfdbe
carolinedanzi <cdanzi@ephrataschools.org>
2020-09-21 01:54:06 +0000

    Initial Rocket class

---
+++ sampleCode/Chapter1/Rocket.java
@@ -0,0 +1,42 @@
+package sampleCode.Chapter1;
+
+/**
+ * Prints the following image to the console.
+ *    /\
+ *   /  \
+ *  /    \
+ *  \    /
+ *   \  /
+ *    \/
+ *  \    /
+ *   \  /
+ *    \/
+ *    /\
+ *   /  \
+ *  /    \
+ *    /\
+ *   /  \
+ *  /    \
+ * +------+
+ * |      |
+ * |      |
+ * +------+
+ * |United|
+ * |States|
+ * +------+
+ * |      |
+ * |      |
+ * +------+
+ *    /\
+ *   /  \
+ *  /    \
+ */
+public class Rocket {
+    public static void main(String[] args) {
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+
+    }
+}
