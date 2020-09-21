84320c50b95bb089158345e7f6ff53338a88cdcc
carolinedanzi <cdanzi@ephrataschools.org>
2020-09-21 04:10:14 +0000

    Updates to Rocket lab

--- sampleCode/Chapter1/Rocket.java
+++ sampleCode/Chapter1/Rocket.java
@@ -1,42 +1,60 @@
 package sampleCode.Chapter1;
 
 /**
- * Prints the following image to the console.
- *    /\
- *   /  \
- *  /    \
- *  \    /
- *   \  /
- *    \/
- *  \    /
- *   \  /
- *    \/
- *    /\
- *   /  \
- *  /    \
- *    /\
- *   /  \
- *  /    \
- * +------+
- * |      |
- * |      |
- * +------+
- * |United|
- * |States|
- * +------+
- * |      |
- * |      |
- * +------+
- *    /\
- *   /  \
- *  /    \
+ * Prints ASCII images, such as a rocket, to the console.
+ * 
+ * @author [YOUR NAME HERE]
  */
 public class Rocket {
-    public static void main(String[] args) {
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
 
+    /*
+     * ROCKET LAB INSTRUCTIONS
+     * 
+     * We are going to write a program to print the following to
+     * the console:
+     * 
+     *    /\
+     *   /  \
+     *  /    \
+     *  \    /
+     *   \  /
+     *    \/
+     *  \    /
+     *   \  /
+     *    \/
+     *    /\
+     *   /  \
+     *  /    \
+     *    /\
+     *   /  \
+     *  /    \
+     * +------+
+     * |      |
+     * |      |
+     * +------+
+     * |United|
+     * |States|
+     * +------+
+     * |      |
+     * |      |
+     * +------+
+     *    /\
+     *   /  \
+     *  /    \
+     * 
+     * Extra: If you finish early, try any of the below challenges:
+     * A. Make a new picture using the shapes from this lab.
+     * B. Create a static method to draw a new shape (ex: circle).
+     *    Use it to draw a new picture.
+     * C. Make your own ASCII art! Use Pikachu for inspiration or
+     *    find examples online.
+     */
+
+    public static void main(String[] args) {
+        // The code below is just to get you started.
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
     }
 }
