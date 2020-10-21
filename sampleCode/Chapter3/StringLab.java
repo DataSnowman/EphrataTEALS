7fd8e82e96b8e0f53c696c13849b6ae8d31e544d
Caroline Danzi <cdanzi@google.com>
2020-10-20 19:19:26 -0700

    Add StringLab outline for students

---
+++ sampleCode/Chapter3/StringLab.java
@@ -0,0 +1,13 @@
+// Remember to change the package name!
+package sampleCode.Chapter3;
+
+/**
+ * Prints out the result of String methods.
+ */
+public class StringLab {
+    
+    public static void main(String[] args) {
+        System.out.print("The result of your test is: ");
+        System.out.println(/* Test case goes here */);
+    }
+}
