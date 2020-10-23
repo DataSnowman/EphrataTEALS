3cf014b02a6827b4ae276b435045e59fd89d340b
Brian Rogers <brian-dot-net@outlook.com>
2020-10-23 10:34:19 -0700

    Simplify even more!

--- sampleCode/Chapter3/Scanner/TipCalculatorParamsScanner.java
+++ sampleCode/Chapter3/Scanner/TipCalculatorParamsScanner.java
@@ -7,31 +7,21 @@ public class TipCalculatorParamsScanner {
 
         Scanner console = new Scanner(System.in);
 
-        // Prompt for total money
-        System.out.println("Enter Brian's Money in wallet: ");
-        double brianMoney = console.nextDouble();
-
         // Prompt for meal cost
-        System.out.println("Enter Brian's Meal cost: ");
+        System.out.print("Enter Brian's Meal cost: ");
         double brianMeal = console.nextDouble();
 
-        // Calculate the tip and show money left
-        brianMoney -= calculateTip(brianMeal);
-        System.out.println("Brian has $" + brianMoney + " in his wallet");
+        // Prompt for tip %
+        System.out.print("Enter Brian's tip %: ");
+        int brianTipPercentage = console.nextInt();
 
-        console.close();//write at end of code.
+        // Calculate the tip
+        double totalWithTip -= calculateTip(brianMeal, brianTipPercentage);
+        System.out.println("Brian will pay $" + totalWithTip + " including tip");
     }
-
-    // Overloaded method definition for `calculateTip`
-    public static double calculateTip(double mealTotal) {
-        return calculateTip(mealTotal, 10.0); // assume 10%
-    }
-
-    public static double calculateTip(double mealTotal, double tipPercentage) {
-        System.out.println("Your total without tip is $" + mealTotal);
-        System.out.println("You chose to tip " + tipPercentage + "%");
+    
+    public static double calculateTip(double mealTotal, int tipPercentage) {
         mealTotal *= 1 + (tipPercentage / 100);
-        System.out.println("Your meal total is $" + mealTotal);
         return mealTotal;
     }
 }
