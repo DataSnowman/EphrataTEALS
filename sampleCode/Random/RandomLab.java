d931f29cdcfba2dc9ef6a48ba8700d59a757a00e
carolinedanzi <carolinedanzi@outlook.com>
2021-01-26 06:24:25 +0000

    Add code for Random lab

---
+++ sampleCode/Random/RandomLab.java
@@ -0,0 +1,28 @@
+package sampleCode.Random;
+
+/** 
+ * Lab from 01/27: Using the Random class in Java to simulate spells.
+ */
+public class RandomLab {
+    
+    public static void main(String[] args) {
+        // 1. Choose a spell from the list provided. Spell chosen: ______
+
+        // 2. For your spell, calculate the damage range. For example, for a spell
+        // that does 1d6 fire damage, the damage range is 1 to 6
+        // Damage range: ______ to _______
+
+        // 3. Use Random to calculate the damage for one casting of the spell.
+        // Print out the damage dealt.
+
+        // 4. Player's choice! Expand your program - be as creative as you'd like
+        // and/or choose one of the following:
+        //      a. Implement the "Advanced" feature for your chosen spell
+        //      b. Add print statements to tell a story featuring your spell
+        //      c. In D&D, you roll a d20 to see if your spell hits its target.
+        //         If you roll a 10 or higher, your spell hits! If not, it
+        //         misses and no damage is dealt.
+        //      d. Cast your spell multiple times (loop)
+        //      e. Incorporate user interaction
+    }
+}
