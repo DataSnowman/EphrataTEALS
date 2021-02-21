c4c12146413923fd1620220e10528dc13cb7a67e
DataSnowman <darsch@microsoft.com>
2021-02-21 22:08:29 +0000

    Camping

---
+++ sampleCode/Chapter7/Camping.java
@@ -0,0 +1,78 @@
+package sampleCode.Chapter7;
+
+import java.util.Arrays;
+
+public class Camping {
+    public static void main(String[] args) {
+    
+      // dogNames array here
+      
+
+
+      
+      // babyGrowls array here and call countBabyGrowls Method
+
+
+
+       
+      // harlowNaps array here and call increaseNap Method as well as minHarlowNaps and maxHarlowNaps Methods
+      
+
+
+      
+      // holdHudson array here and call sumHudsonHold Method
+      
+
+      // Println statements here
+      /* Should output
+      Baby growled 43 times!
+      Harlow napped Minimum: 0.5 Maximum: 3.25
+      Hudson was held 6 times!
+      */
+
+      
+
+
+    } //end of main method
+    
+    // countBabyGrowls static method here
+    
+
+
+
+
+
+
+    
+    // increaseNap static method here
+    
+
+
+
+    
+    // minHarlowNaps static method here     
+    
+
+
+
+
+
+
+
+    
+    // maxHarlowNaps static method here    
+    
+
+
+
+    
+    
+    // sumHudsonHold static method here  
+    
+
+
+
+
+
+    
+}
