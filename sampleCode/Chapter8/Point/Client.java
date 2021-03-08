3d733bef0bbc23c37f3961dc2113161466faf5b4
DataSnowman <darsch@microsoft.com>
2021-03-08 05:08:13 +0000

    format

--- sampleCode/Chapter8/Point/Client.java
+++ sampleCode/Chapter8/Point/Client.java
@@ -7,13 +7,16 @@ public class Client {
         Point p1 = new Point();
         p1.x = 3;
         p1.y = 5;
+
         Point p2 = new Point();
         p2.x = 12;
         p2.y = 4;
 
+        // For p1 Subtract 1 from x and 2 from y
+        // For p2 Add 6 to x and 8 to y
         p1.translate(-1, -2);
         p2.translate(6, 8);
 
     }
-    
+
 }
