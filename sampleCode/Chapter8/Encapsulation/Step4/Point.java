fa78e0257d2ea800ebfbeba7174d1d976d5bfa87
Brian Rogers <brian-dot-net@outlook.com>
2021-03-16 13:39:07 -0700

    Encapsulation (step 4 - Point class)

---
+++ sampleCode/Chapter8/Encapsulation/Step4/Point.java
@@ -0,0 +1,12 @@
+package sampleCode.Chapter8.Encapsulation.Step4;
+
+public class Point {
+    double x;
+    double y;
+
+    // Initial state
+    public Point(double initX, double initY) {
+        x = initX;
+        y = initY;
+    }
+}
