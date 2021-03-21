81a0c7f4739624817e333e60cf470c94e88a9e18
Ethan Roday <ethan.roday@gmail.com>
2021-03-21 14:53:27 -0700

    Create CardShuffler.java

---
+++ sampleCode/Chapter8/CardLab/CardShuffler.java
@@ -0,0 +1,13 @@
+package studentWork.CardLab;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CardShuffler {
+    public Card[] shuffle(Card[] array) {
+        List<Card> list = Arrays.asList(array);
+        Collections.shuffle(list);
+		return list.toArray(array);
+    }
+}
